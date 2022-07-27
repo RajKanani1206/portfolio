eb27f9f051557f886068cb3cff704a8c2052308b
Raj Kanani <rajkanani1206@gmail.com>
2022-07-27 21:57:53 +0530

    Added home banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/HomeBanner";
 
 const App = () => {
   return (
-    <div className="app">
-      <Header />
-      <main className="app-body"></main>
-    </div>
+    <>
+      <div className="main-banner">
+        <Header />
+        <Home />
+      </div>
+    </>
   );
 };
 
