6b13919615a2366e33d71836c4b4a8812605d3f1
Raj Kanani <rajkanani1206@gmail.com>
2022-07-30 23:55:31 +0530

    Minor changes fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,39 @@
+import { useState } from "react";
 import { Navbar, Container, Offcanvas, Nav } from "react-bootstrap";
 import "./style.css";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Navbar expand="md" variant="dark">
       <Container>
         <Navbar.Brand className="d-flex align-items-center">
           <img alt="Logo" src="images/logo-white.svg" width="60" height="60" className="d-inline-block logo-img" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} className="menu-button" />
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} className="menu-button" onClick={handleShow} />
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-md`}
           aria-labelledby={`offcanvasNavbarLabel-expand-md`}
           placement="end"
+          scroll={true}
+          show={show}
+          onHide={handleClose}
         >
           <Offcanvas.Header closeButton className="p-4">
             <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`} className="text-black"></Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body className="mt-5 mt-md-0">
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <a href="#a" className="text-decoration-none menu-items mx-4 my-3 my-md-0">
+              <a href="#a" className="text-decoration-none menu-items mx-4 my-3 my-md-0" onClick={handleClose}>
                 About Me
               </a>
-              <a href="#d" className="text-decoration-none menu-items mx-4 my-3 my-md-0">
+              <a href="#d" className="text-decoration-none menu-items mx-4 my-3 my-md-0" onClick={handleClose}>
                 Skills
               </a>
-              <a href="#d" className="text-decoration-none menu-items mx-4 my-3 my-md-0">
+              <a href="#d" className="text-decoration-none menu-items mx-4 my-3 my-md-0" onClick={handleClose}>
                 My Work
               </a>
             </Nav>
