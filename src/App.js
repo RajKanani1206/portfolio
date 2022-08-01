99a86882a3cba36a8aff588d64d665efc45a7635
Raj Kanani <rajkanani1206@gmail.com>
2022-08-01 13:38:17 +0530

    Added About Me section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import Header from "./components/Header";
 import Home from "./components/HomeBanner";
 
@@ -7,6 +8,7 @@ const App = () => {
     <>
       <Header />
       <Home />
+      <About />
     </>
   );
 };
---
+++ src/components/About/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+import "./style.css";
+
+const About = () => {
+  return (
+    <div className="about-section text-center text-black my-5" id="about">
+      <h1 className="about-heading mb-5">About Me</h1>
+      <Container>
+        <div className="about-content d-flex justify-content-between align-items-center flex-column flex-md-row">
+          <div className="col-left mb-4 mb-md-0">
+            <img src="images/profile.jpg" alt="Profile" />
+          </div>
+          <div className="col-right text-start p-4 p-md-0">
+            <p className="font-monospace fst-italic">
+              I am a Software Developer specializing in Front-End who likes to build blazing fast websites. I love
+              contributing to open source projects. I have worked mostly with ReactJS. Checkout my work below and feel
+              free to get in touch with me if you have some interesting projects.
+            </p>
+            <Button variant="plain-text" className="text-white border-0">
+              Resume
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default About;
