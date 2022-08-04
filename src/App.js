3bcac658ef16697728446df02f6558fe838d2af0
Raj Kanani <rajkanani1206@gmail.com>
2022-08-04 17:59:10 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/HomeBanner";
 import Skills from "./components/Skills";
@@ -11,6 +12,7 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Footer />
     </>
   );
 };
