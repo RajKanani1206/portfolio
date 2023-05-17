ac9ae3ccb85eddc6682bb94829b4560e036660d7
Raj Kanani <rajkanani1206@gmail.com>
2023-05-17 16:52:39 +0200

    Updated Projects

--- src/components/Projects/projectDetails.js
+++ src/components/Projects/projectDetails.js
@@ -1,6 +1,12 @@
 // eslint-disable-next-line
 export default [
   {
+    title: "Pin The Trip",
+    subTitle: "The Travel Partner That You Needed.",
+    webLink: "https://pinthetrip.onrender.com",
+    githubLink: "https://github.com/RajKanani1206/pin-the-trip",
+  },
+  {
     title: "SortOut",
     subTitle: "The Next Generation Sorting Visualizer.",
     webLink: "https://rajkanani1206.github.io/SortOut/",
